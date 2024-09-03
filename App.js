5c7a4ffa494460847ac662dc1f61cb66c60d0953
Hari <krishnahari6622@gmail.com>
2024-09-03 23:54:10 +0530

    Initial Project Setup
    Added
    - Eslint
    - Redux
    - Navigation

---
+++ App.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
