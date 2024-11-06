803b17b8cd3a700b1f6183e65037dcad177d6bdc
Suhasini9663 <suhasini.ganaka@gmail.com>
2024-11-06 18:13:41 +0530

    WIP:selftrip

--- src/screens/bottomTab/SelfTrip.js
+++ src/screens/bottomTab/SelfTrip.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppHeader from '../../components/AppHeader';
+import CustomText from '../../components/ui/CustomText';
+import CustomInput from '../../components/ui/CustomInput';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
+
+const [customerName, setCustomerName] = useState('');
+const [customerPhone, setCustomerPhone] = useState('');
+
+
+
+const renderSelfTripContent = () => {
+  <>
+    <View style={{ ...styles.sectionContainer, gap: 10 }}>
+      <CustomText text={'Customer Details :'} variant={'sectionTitleText'} />
+      <CustomInput
+        placeholder="customer Name"
+        value={customerName}
+        onChangeText={setCustomerName}
+      />
+      <CustomInput
+        placeholder="customer Phone"
+        value={customerPhone}
+        onChangeText={setCustomerPhone}
+      />
+
+
+    </View>
+
+  </>
+}
 const SelfTrip = () => {
   return (
     <>
@@ -10,12 +39,38 @@ const SelfTrip = () => {
         groupIcon={true}
         onlineIcon={true}
         muteIcon={true}
-        search={true}
-      />
+        search={true} />
+
+      <KeyboardAwareScrollView
+        style={styles.container}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          styles.contentContainer,
+          { backgroundColor: theme.backgroundColor },
+        ]}
+        enableOnAndroid={true}
+        enableAutomaticScroll={true}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View>
+          <CustomSelect />
+          text={'selfTrip'}
+        </View>
+      </KeyboardAwareScrollView>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'F3F5FD',
+    padding: 20,
+  },
+  contentContainer: {
+    paddingBottom: 20,
+  }
+
+});
 
 export default SelfTrip;
